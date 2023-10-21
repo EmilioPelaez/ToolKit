8c96a220daae8ce055d771e47e26149f3bfab994
Emilio Peláez <me@epelaez.net>
2023-10-21 02:02:54 +0200

    Rename minHeight to minSize

--- Sources/UIToolKit/Modifiers/MinHeight.swift
+++ Sources/UIToolKit/Modifiers/MinHeight.swift
@@ -9,8 +9,8 @@ public extension View {
 	 Utilizes the view provided by the closure to establish a minimum height for this view. The view
 	 provided by the closure is rendered at 0 opacity
 	 */
-	func minHeight<V: View>(_ closure: @escaping () -> V) -> some View {
-		ZStack {
+	func minSize<V: View>(alignment: Alignment = .center, closure: @escaping () -> V) -> some View {
+		ZStack(alignment: alignment) {
 			closure()
 				.opacity(0)
 			self
